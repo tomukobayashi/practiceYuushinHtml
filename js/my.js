39a8e583b83d3737b4af0a541e7f639659235366
cyou-kobayashi <scyoutec.34@gmail.com>
2020-03-23 17:17:37 +0900

    first commit

---
+++ js/my.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$('.slider').bxSlider({
+		auto: true,
+		pagerCustom: '.custom-thumb',
+		controls: true,
+		nextSelector: "#feed-next-btn",
+        prevSelector: "#feed-prev-btn"
+	});
+});
+
+//トップページアクセス時にロゴ表示
+$(function() {
+  setTimeout(function(){
+    $('.start p').fadeIn(1600);
+  },500); //0.5秒後にロゴをフェードイン
+  setTimeout(function(){
+    $('.start').fadeOut(500);
+  },2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト
+});
+
+function modalfnc(no) {
+	var id = `js-popup${no}`;
+	var popup = document.getElementById(id);
+	if(!popup) return;
+	popup.classList.toggle('is-show');
+}
